66d58840ee05b8f89da1295e6b2d8a197cc2d0e7
Jamie Cui <jamie.cui@outlook.com>
2025-05-08 22:25:03 +0800

    many tweaks

--- init.el
+++ init.el
@@ -15,7 +15,7 @@
   (setq jc-emacs-directory "~/Desktop/emacs.d"))
 
 ;; load theme
-(load-theme 'wombat t)
+;; (load-theme 'wombat t)
 
 ;; defining your own
 ;; macos: ~/Library/Mobile Documents/com~apple~CloudDocs/org-root
@@ -38,8 +38,47 @@
 ;; maximize on startup
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
-;; scroll should preserve screen position
-(setq scroll-preserve-screen-position t)
+;; scrolling
+(setq hscroll-margin 2
+      hscroll-step 1
+      ;; Emacs spends too much effort recentering the screen if you scroll the
+      ;; cursor more than N lines past window edges (where N is the settings of
+      ;; `scroll-conservatively'). This is especially slow in larger files
+      ;; during large-scale scrolling commands. If kept over 100, the window is
+      ;; never automatically recentered. The default (0) triggers this too
+      ;; aggressively, so I've set it to 10 to recenter if scrolling too far
+      ;; off-screen.
+      scroll-conservatively 10
+      scroll-margin 0
+      scroll-preserve-screen-position t
+      ;; Reduce cursor lag by a tiny bit by not auto-adjusting `window-vscroll'
+      ;; for tall lines.
+      auto-window-vscroll nil
+      ;; mouse
+      mouse-wheel-scroll-amount '(2 ((shift) . hscroll))
+      mouse-wheel-scroll-amount-horizontal 2)
+
+;; The blinking cursor is distracting, but also interferes with cursor settings
+;; in some minor modes that try to change it buffer-locally (like treemacs) and
+;; can cause freezing for folks (esp on macOS) with customized & color cursors.
+(blink-cursor-mode -1)
+
+;; Don't blink the paren matching the one at point, it's too distracting.
+(setq blink-matching-paren nil)
+
+;; Don't stretch the cursor to fit wide characters, it is disorienting,
+;; especially for tabs.
+(setq x-stretch-cursor nil)
+
+;; Reduce the clutter in the fringes; we'd like to reserve that space for more
+;; useful information, like diff-hl and flycheck.
+(setq indicate-buffer-boundaries nil
+      indicate-empty-lines nil)
+
+;; Expand the minibuffer to fit multi-line text displayed in the echo-area. This
+;; doesn't look too great with direnv, however...
+(setq resize-mini-windows 'grow-only
+      tooltip-resize-echo-area t)
 
 ;; do not show me native-comp warning and erros
 (setq native-comp-async-report-warnings-errors 'silent)
@@ -340,7 +379,7 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
   :ensure t
   :custom
   (cnfonts-personal-fontnames '(
-                                ("Sarasa Term SC Nerd" "FiraCode Nerd Font Mono") ;; English
+                                ("Sarasa Term SC Nerd" "FiraCode Nerd Font Mono" "0xProto Nerd Font Mono") ;; English
                                 ("Sarasa Term SC Nerd") ;; Chinese
                                 nil  ;; Ext-B
                                 ("Symbols Nerd Font Mono") ;; Symbol
@@ -356,7 +395,7 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 
 (use-package smartparens
   :ensure t  ;; install the package
-  :hook (prog-mode text-mode markdown-mode org-mode) ;; add `smartparens-mode` to these hooks
+  :hook (prog-mode text-mode markdown-mode org-mode) 
   :config
   ;; load default config
   (require 'smartparens-config))
@@ -372,14 +411,12 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
   :config
   (add-hook 'org-mode-hook 'org-appear-mode))
 
-
-;; see: https://github.com/doomemacs/doomemacs/blob/da32e8e6f233a80d54d51964d21c4b46b000323b/modules/editor/evil/config.el#L324C1-L341C42
 (use-package evil-escape
   :ensure t
   :after (evil general)
   :init
   (setq evil-escape-excluded-states '(normal visual multiedit emacs motion)
-        evil-escape-excluded-major-modes '(neotree-mode treemacs-mode vterm-mode)
+        evil-escape-excluded-major-modes '(vterm-mode)
         evil-escape-key-sequence nil
         evil-escape-delay 0.15)
   :config
@@ -436,7 +473,7 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
           :host "dashscope.aliyuncs.com/compatible-mode/v1"
           :endpoint "/chat/completions"
           :stream t
-          :key "**************************"
+          :key (auth-source-pick-first-password :host "api.deepseek.com")
           :models '(deepseek-r1)))
 
   (setf (alist-get 'org-mode gptel-prompt-prefix-alist) "** @jc\n")
@@ -676,8 +713,7 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 (use-package flycheck-popup-tip
   :ensure t
   :config
-  (add-hook 'flycheck-mode-hook 'flycheck-popup-tip-mode)
-  )
+  (add-hook 'flycheck-mode-hook 'flycheck-popup-tip-mode))
 
 (use-package apheleia
   :ensure t
