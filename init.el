76d39666091f2b8d66b993b22efd3b6d27450f18
Jamie <jamie.cui94@gmail.com>
2025-06-11 17:21:16 +0800

    Update init.el

--- init.el
+++ init.el
@@ -276,3 +276,6 @@
 (use-package org-imgtog
   :load-path (lambda () (concat jc-emacs-directory "/site-lisp"))
   :hook org-mode)
+
+(+ensure-packages-installed '(persp-projectile))
+(use-package persp-projectile)
