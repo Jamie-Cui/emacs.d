882213c7758d345f786e4a1418fbd2ebce7f29c5
Jamie <jamie.cui94@gmail.com>
2025-06-20 11:17:45 +0800

    Create dir-locals-eglot-workspace.el

---
+++ dir-locals-eglot-workspace.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c++-ts-mode . 
+              ((eglot-workspace-configuration . 
+                                              (:clangd (:server-executable "clangd"))
+                                              ))
+              ))
