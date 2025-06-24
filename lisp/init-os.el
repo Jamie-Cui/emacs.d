2d4382681546a7ad73c1e6c3e837588d3640e62f
Jamie <jamie.cui94@gmail.com>
2025-06-24 16:33:16 +0800

    Update init-os.el

--- lisp/init-os.el
+++ lisp/init-os.el
@@ -49,4 +49,16 @@
   (setq insert-directory-program "gls" dired-use-ls-dired t)
   )
 
+;;; --------------------------------------
+;;; Windows
+;;; --------------------------------------
+
+(when (eq system-type 'windows-nt)
+  (setq tramp-default-method "plink")
+  (prefer-coding-system 'utf-8)
+  (set-terminal-coding-system 'utf-8)
+  (set-keyboard-coding-system 'utf-8)
+  (set-language-environment "UTF-8")
+  )
+
 (provide 'init-os)
