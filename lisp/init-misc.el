2b4c74fe9e2fa0c729f47bf2f29a9e1f3c8b8cd5
Jamie Cui <jamie.cui@outlook.com>
2025-06-25 13:46:00 +0800

    fix

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -154,28 +154,18 @@
 ;; fancy eshell 
 ;; see: https://lambdaland.org/posts/2024-08-19_fancy_eshell_prompt/
 (setopt eshell-prompt-function '+eshell/fancy-shell)
-;; (setopt eshell-prompt-regexp "^[^#$\n]* [$#] ")
+(setopt eshell-prompt-regexp "^[^#$\n]* [$#] ")
 (setopt eshell-highlight-prompt nil)
 
 (defun +eshell/fancy-shell ()
   "A pretty shell with git status"
   (let* ((cwd (abbreviate-file-name (eshell/pwd)))
-         (ref (magit-get-shortname "HEAD"))
-         (stat (magit-file-status))
-         (x-stat eshell-last-command-status)
-         (git-chunk
-          (if ref
-              (format "%s%s%s "
-                      (propertize (if stat "[" "(") 'font-lock-face (list :foreground (if stat "red" "green")))
-                      (propertize ref 'font-lock-face '(:foreground "yellow"))
-                      (propertize (if stat "]" ")") 'font-lock-face (list :foreground (if stat "red" "green"))))
-            "")))
+         (x-stat eshell-last-command-status))
     (propertize
-     (format "%s %s %s$ "
+     (format "%s %s $ "
              (if (< 0 x-stat) (format (propertize "!%s" 'font-lock-face '(:foreground "red")) x-stat)
                (propertize "➤" 'font-lock-face (list :foreground (if (< 0 x-stat) "red" "green"))))
-             (propertize cwd 'font-lock-face '(:foreground "#45babf"))
-             git-chunk)
+             (propertize cwd 'font-lock-face '(:foreground "#45babf")))
      ;; 'read-only t
      'front-sticky   '(font-lock-face read-only)
      'rear-nonsticky '(font-lock-face read-only))))
