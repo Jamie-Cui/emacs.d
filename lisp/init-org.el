2f186b198786afd86d41d603c5bcb25b41e23260
Jamie <jamie.cui94@gmail.com>
2025-06-30 16:18:41 +0800

    Update init-org.el

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -27,7 +27,26 @@
    deft
    ;; allow drawing
    plantuml-mode
+   ;; presentation
+   org-present
    ))
+
+(use-package org-present
+  :ensure t
+  :config
+  (add-hook 'org-present-mode-hook
+            (lambda ()
+              (org-present-big)
+              (org-display-inline-images)
+              (org-present-hide-cursor)
+              (org-present-read-only)))
+  (add-hook 'org-present-mode-quit-hook
+            (lambda ()
+              (org-present-small)
+              (org-remove-inline-images)
+              (org-present-show-cursor)
+              (org-present-read-write)))
+  )
 
 (use-package org
   :custom
