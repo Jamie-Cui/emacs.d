555f16663909729996128a834dd615db26306b63
Jamie <jamie.cui94@gmail.com>
2025-07-16 10:47:01 +0800

    Update init-os.el

--- lisp/init-os.el
+++ lisp/init-os.el
@@ -59,7 +59,7 @@
   (setq inhibit-eol-conversion t)
   (prefer-coding-system 'utf-8)
   (setq buffer-file-coding-system 'utf-8-unix)
-  ;;(set-terminal-coding-system 'utf-8)
+  (set-terminal-coding-system 'utf-8)
   ;;(set-keyboard-coding-system 'utf-8)
   (set-language-environment "UTF-8")
   )
