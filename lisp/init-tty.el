ce97ee61e8eaf320ec576d9f6d9779782aed18b4
Jamie <jamie.cui94@gmail.com>
2025-07-28 10:11:06 +0800

    Update init-tty.el

--- lisp/init-tty.el
+++ lisp/init-tty.el
@@ -4,17 +4,28 @@
 
 (require 'init-utils)
 
-;; Support for child frames in terminal frames was added in 31. Enable it, if it
-;; is available.
-(when (featurep 'tty-child-frames)
-  (add-hook 'tty-setup-hook #'tty-tip-mode))
+(unless (display-graphic-p)
+  (+ensure-packages-installed
+   '(
+     corfu-terminal
+     ))
 
-;; Use OSC52 protocol, which is used by alacritty by default
-(defun copy-to-system-clipboard (text &optional push)
-  (send-string-to-terminal (format "\e]52;c;%s\a" (base64-encode-string text))))
+  ;; Support for child frames in terminal frames was added in 31. Enable it, if it
+  ;; is available.
+  (when (featurep 'tty-child-frames)
+    (add-hook 'tty-setup-hook #'tty-tip-mode))
 
-(when (not (display-graphic-p))
-  (setq interprogram-cut-function 'copy-to-system-clipboard))
+  ;; Use OSC52 protocol, which is used by alacritty by default
+  (defun copy-to-system-clipboard (text &optional push)
+    (send-string-to-terminal (format "\e]52;c;%s\a" (base64-encode-string text))))
 
+  (setq interprogram-cut-function 'copy-to-system-clipboard)
+
+  (use-package corfu-terminal
+    :ensure t
+    :config
+    (corfu-terminal-mode +1)
+    )
+  )
 
 (provide 'init-tty)
