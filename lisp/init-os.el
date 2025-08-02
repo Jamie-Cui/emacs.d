85b10378904e4411ca6c950df54475c04bef22a3
jamie cui <jamie.cui@outlook.com>
2025-08-02 23:45:13 +0800

    chore(init-os): comment out magic keyboard meta key setting
    The commit comments out the line that sets the meta key for Magic
    Keyboard users, while preserving the note about its relevance.

--- lisp/init-os.el
+++ lisp/init-os.el
@@ -75,7 +75,7 @@
      "powershell.exe -Command \"(Get-Clipboard -Format image).Save('$(wslpath -w %s)')\""))
   )
 
-;; if you are using magic keyboard
-(setq x-super-keysym 'meta)
+;; NOTE if you are using magic keyboard
+;; (setq x-super-keysym 'meta)
 
 (provide 'init-os)
