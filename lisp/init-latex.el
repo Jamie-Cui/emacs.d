b0e594ae4d7378d9af1df2921273128bf1ac5b1d
Jamie Cui <jamie.cui94@gmail.com>
2025-08-28 16:57:29 +0800

    Update init-latex.el

--- lisp/init-latex.el
+++ lisp/init-latex.el
@@ -3,6 +3,7 @@
 ;;; Code:
 
 (require 'init-org)
+(require 'reftex)
 
 (+package/ensure-install
  '(
@@ -92,11 +93,39 @@
       (find-file file)))
 
   ;; Configure uniquification (appends a/b/c for duplicates)
-  (setq bibtex-autokey-add-year t)
-  (setq bibtex-autokey-year-length 4)
-  (setq bibtex-autokey-titlewords 0)
-  (setq bibtex-autokey-titlewords-stretch 0)
-  (setq bibtex-autokey-name-year-separator "")
+  (setopt bibtex-autokey-name-case-convert-function #'upcase)
+  (setopt bibtex-autokey-names 4)
+  (setopt bibtex-autokey-additional-names "+")
+  (setopt bibtex-autokey-name-stretch 0)
+  (setopt bibtex-autokey-name-length 1)
+  (setopt bibtex-autokey-year-length 4)
+  (setopt bibtex-autokey-titlewords 0)
+  (setopt bibtex-autokey-titlewords-stretch 0)
+  (setopt bibtex-autokey-name-year-separator "")
+
+  ;; HACK override this function from built-in
+  ;; maybe report upstream?
+  (defun bibtex-autokey-abbrev (string len)
+    "Return an abbreviation of STRING with at least LEN characters.
+If LEN is positive the abbreviation is terminated only after a consonant
+or at the word end.  If LEN is negative the abbreviation is strictly
+enforced using abs (LEN) characters.  If LEN is not a number, STRING
+is returned unchanged."
+    (cond ((or (not (numberp len))
+               (<= (length string) (abs len)))
+           string)
+          ((equal len 0)
+           "")
+          ((equal len 1)
+           (substring string 0 1))
+          ((< len 0)
+           (substring string 0 (abs len)))
+          (t (let* ((case-fold-search t)
+                    (abort-char (string-match "[^aeiou]" string (1- len))))
+               (message "%s" abort-char)
+               (if abort-char
+                   (substring string 0 (1+ abort-char))
+                 string)))))
 
   ;; default bib file
   (add-to-list 'bibtex-files (concat +emacs/org-root-dir "/all-ref.bib"))
@@ -106,6 +135,7 @@
   :ensure t
   :config
   (add-to-list 'citar-bibliography (concat +emacs/org-root-dir "/all-ref.bib"))
+  (add-to-list 'citar-library-paths (concat +emacs/org-root-dir "/pdf"))
   (add-to-list 'citar-notes-paths (concat +emacs/org-root-dir "/roam"))
   ;; NOTE this var is used by org-export
   (add-to-list 'org-cite-global-bibliography (concat +emacs/org-root-dir "/all-ref.bib"))
