b48a5a74d46844fc5607290446c6212e6618b623
Jamie Cui <jamie.cui@outlook.com>
2025-08-30 13:54:57 +0800

    feat(init.el): add cmake-ts-mode for CMakeLists.txt files
    Add CMakeLists.txt pattern to auto-mode-alist to automatically use
    cmake-ts-mode for CMake configuration files.

--- init.el
+++ init.el
@@ -193,6 +193,7 @@
   (rassq-delete-all 'c-or-c++-mode auto-mode-alist)
   (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-ts-mode))
   (add-to-list 'auto-mode-alist '("\\.cc\\'" . c++-ts-mode))
+  (add-to-list 'auto-mode-alist '("CMakeLists\\.txt\\'" . cmake-ts-mode))
 
   ;; HACK toggle mode automatically
   (defun +treesit-auto/toggle ()
