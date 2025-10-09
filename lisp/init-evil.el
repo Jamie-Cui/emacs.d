9f1741f67cd5896fb3d176c528f9610b937b8826
Jamie Cui <jamie.cui94@gmail.com>
2025-10-09 15:36:28 +0800

    Update init-evil.el

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -37,6 +37,7 @@
   ;; whether to use emacs bindings in insert-mode
   (setq evil-disable-insert-state-bindings nil)
   :config
+  (setopt evil-want-C-i-jump nil) ; HACK for tty org-mode, make TAB works!
   (defalias #'forward-evil-word #'forward-evil-symbol)
   ;; make evil-search-word look for symbol rather than word boundaries
   (setq-default evil-symbol-word-search t)
