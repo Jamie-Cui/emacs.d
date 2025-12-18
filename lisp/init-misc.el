6083dc181b9c1f4b97bd7f8cfa003f0b96685bd2
Jamie Cui <jamie.cui@outlook.com>
2025-12-18 12:55:32 +0800

    fix(init-misc): enable electric-indent-mode disabling
    Uncommented the lines that disable electric-indent-mode and add the
    hook to ensure it stays disabled after major mode changes.

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -59,8 +59,8 @@
               (list ".*" auto-save-list-file-prefix t)))
 
 ;; disable electric-indent-mode, forever
-;; (electric-indent-mode -1)
-;; (add-hook 'after-change-major-mode-hook (lambda() (electric-indent-mode -1)))
+(electric-indent-mode -1)
+(add-hook 'after-change-major-mode-hook (lambda() (electric-indent-mode -1)))
 
 ;; maximize on startup
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
